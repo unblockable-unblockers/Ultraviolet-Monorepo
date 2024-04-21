e5d44b35d9af872f09f39970d53ee53c2f139549
Parker Wahle <regulad@regulad.xyz>
2024-04-21 18:05:00 -0400

    stupidRPC

--- packages/Ultraviolet-Static/public/index.js
+++ packages/Ultraviolet-Static/public/index.js
@@ -25,7 +25,19 @@ const errorCode = document.getElementById("uv-error-code");
  * @returns {*|boolean|string}
  */
 function getUvUrl() {
-  return document.getElementById("uv-frame").src;
+  return getFrame().src;
+}
+
+function getDecodedUrl() {
+  return __uv$config.decodeUrl(getUvUrl().slice(__uv$config.prefix));
+}
+
+function getFrameHolder() {
+  return document.getElementById("uv-frame-holder");
+}
+
+function getFrame() {
+  return document.getElementById("uv-frame");
 }
 
 /**
@@ -40,11 +52,51 @@ async function setUvUrl(url) {
     errorCode.textContent = err.toString();
     throw err;
   }
-
-  let frameHolder = document.getElementById("uv-frame-holder");
+  console.log("Navigating", url);
+  let frameHolder = getFrameHolder();
   frameHolder.style.display = "block";
-  let frame = document.getElementById("uv-frame");
+  let frame = getFrame();
   frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+}
+
+/*
+ * Messages from the UV frame to operate the UV frame
+ */
+window.onmessage = async (event) => {
+  // this is essentially our own bootleg https://www.jsonrpc.org/specification
+  const data = event.data;
+
+  if (!(data.hasOwnProperty("type") && data.type === "uv")) {
+    return; // not for us
+  }
+
+  const {
+    opcode,
+    payload
+  } = data;
+
+  console.log("Received message", opcode, payload);
+
+  // example:
+  // window.top.postMessage({type:"uv",opcode:"search",payload:"amazon.com"}, "*");
+
+  switch (opcode) {
+    case "getUrl":
+      event.source.postMessage({
+        type: "uv",
+        opcode: "getUrlRet",
+        payload: getDecodedUrl(),
+      });
+      break;
+    case "navigate":
+      await setUvUrl(payload);
+      break;
+    case "search":
+      const url = search(payload, searchEngine.value);
+      await setUvUrl(url);
+      break;
+    // purposely no method to leave the iframe, once we are here we are here for the long haul
+  }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -53,6 +105,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     await setUvUrl(maybeSetUrl);
   }
 });
+
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const url = search(address.value, searchEngine.value);
